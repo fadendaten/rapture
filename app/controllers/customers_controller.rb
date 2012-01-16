aba120305b4f5162b30bb14c0fb0506f29f2edf9
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-16 15:08:08 +0100

    Added more css

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,17 @@
 class CustomersController < ApplicationController
   
   def index
-    
+    @title = "Home"
   end
   
   def new
+    @title = "Kunden erfassen"
     @customer = Customer.new
     
   end
   
   def show
-    
+    @title = "Kundenname" #provisorisch
   end
 
 end
