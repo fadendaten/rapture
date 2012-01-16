11b5eae4dda899b0a2fa47df0fb08d847c7efa3e
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-16 15:40:39 +0100

    Customer can now be created

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,12 +2,22 @@ class CustomersController < ApplicationController
   
   def index
     @title = "Home"
+    @customers = Customer.all
   end
   
   def new
     @title = "Kunden erfassen"
     @customer = Customer.new
-    
+  end
+  
+  def create
+    @customer = Customer.new(params[:customer])
+    if @customer.save
+      redirect_to root_path
+    else
+      @title = "Kunden erfassen"
+      render 'new'
+    end
   end
   
   def show
