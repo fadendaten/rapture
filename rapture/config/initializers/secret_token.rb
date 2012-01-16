4202c8dca6fc1d3878b5e9d1fc88c0244d0cc299
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-16 09:02:37 +0100

    Modified Gemfile

---
+++ rapture/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rapture::Application.config.secret_token = 'abcc45731892613d92d9b30458e20594e169deab4086a947538b04ec7f4d49973a95d728d4b271fff99e4062148656e789366102a592a304083d98741d169645'
