533cc9f4b7cfdd1b0ee5e126bc22650ee597c7dd
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-18 14:50:10 +0100

    Fixed some tests

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -9,28 +9,62 @@ describe CustomersController do
 
   describe "GET 'new'" do
     
-    it "should return http success" do
-      get :new
-      response.should be_success
+    describe "for non-signed in users" do
+      
+      it "should deny access" do
+        get :new
+        response.should redirect_to(signin_path)
+      end
+      
     end
     
-    it "should have the right title" do
-      get :new
-      response.should have_selector( 'title', :content => "Kunden erfassen")
+    describe "for signed in users" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+      end
+      
+      it "should return http success" do
+        get :new
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get :new
+        response.should have_selector( 'title', :content => "Kunden erfassen")
+      end
+      
     end
     
   end
   
   describe "GET 'edit'" do
     
-    it "should return http success" do
-      get :edit, :id => @customer
-      response.should be_success
+    describe "for non-signed in users" do
+      
+      it "should deny access" do
+        get :edit, :id => @customer 
+        response.should redirect_to(signin_path)
+      end
+      
     end
     
-    it "should have the right title" do
-      get :edit, :id => @customer
-      response.should have_selector('title', :content => "#{@customer} editieren | Rapture" )
+    describe "for signed in users" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+      end
+      
+      it "should return http success" do
+        get :edit, :id => @customer
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get :edit, :id => @customer
+        response.should have_selector('title', :content => "#{@customer} editieren | Rapture" )
+      end
+      
     end
     
   end
