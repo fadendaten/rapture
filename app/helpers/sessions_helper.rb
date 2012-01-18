d145dfc7b7dd79ef87bedba45acac2ff6308ea97
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-18 09:48:14 +0100

    Basic signin/out works

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,8 +8,12 @@ module SessionsHelper
     @current_user = user
   end
   
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+  
   def signed_in?
-    !@current_user.nil?
+    !current_user.nil?
   end
   
   def sign_out
