ec3b210d2bc6ba7848864c6e67a486d7fac0bf87
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-20 19:45:05 +0100

    Modified flash message for unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => "YOU ARE NO LONGER WELCOME. GO AWAY!"
+    redirect_to root_url, :alert => "You tried to bypass our security measures. You are no longer welcome in Rapture. Leave and take your loved ones with you."
   end
   
   
