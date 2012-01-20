45ce2632f83b5b87124d37619b9514543e33b448
Simon Kiener <jugglinghobo@gmail.com>
2012-01-20 19:36:21 +0100

    added redirect if user role (CanCan) prevents access to a site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => "YOU ARE NO LONGER WELCOME. GO AWAY!"
+  end
+  
+  
 end
