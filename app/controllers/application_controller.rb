d9e267f98491c06c7dd7d8de72de2eafeaee53a8
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-23 10:50:17 +0100

    Cleaned up String

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, :alert => "You tried to bypass our security measures. You are no longer welcome in Rapture. Leave and take your loved ones with you."
+    security_breach_message = "You tried to bypass our security measures. " +
+                              "You are no longer welcome in Rapture. " +
+                              "Leave and take your loved ones with you."
+    redirect_to root_path, :alert => security_breach_message
   end
   
 end
