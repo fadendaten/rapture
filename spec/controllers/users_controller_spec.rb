d958111ef89663a6ef56848f2a5f6e655af50025
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-27 10:36:34 +0100

    Fixed some tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,38 +1,32 @@
 require 'spec_helper'
 
 describe UsersController do
+  include Devise::TestHelpers
+  render_views
+  
+  before(:each) do
+    @user = Factory(:user)
+    @user.user_roles << Factory(:sudo)
+    sign_in :user, @user
+  end
 
   describe "GET 'new'" do
     it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
+      get :new
       response.should be_success
     end
   end
 
   describe "GET 'edit'" do
     it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
+      get :edit, :id => @user
       response.should be_success
     end
   end
 
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
+      get :show, :id => @user
       response.should be_success
     end
   end
