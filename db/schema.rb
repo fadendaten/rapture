b62b8a36cbe6e06302375f94a886f347b2c77e74
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-01-30 17:11:46 +0100

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -94,7 +94,6 @@ ActiveRecord::Schema.define(:version => 20120130100347) do
     t.string   "last_name"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
