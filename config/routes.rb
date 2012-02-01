2f7d1c4dc3da0266ee5f4d1cdc6cf6a82b03b479
Simon Kiener <jugglinghobo@gmail.com>
2012-02-01 10:58:15 +0100

    fixed routes created by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rapture::Application.routes.draw do
-
-  devise_for :users
-
+  
+  devise_for :users, :path => '' # , :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+  
   root :to => 'customers#index'
   
   resources :customers do
