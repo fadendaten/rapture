9a1f849cea1beaaf6b1a8ffc6ca24574acc1fa5f
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-02-02 14:14:29 +0100

    before felix

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,9 +6,9 @@ class CustomersController < ApplicationController
   
   def index
     @title = "Alle Kunden"
-    if params[:sort]
-      @customers = Customer.sorted(params[:sort]).page(params[:page])
-    elsif params[:search]
+    if !params[:sort].nil?
+      @customers = Customer.sorted(params[:sort], "company ASC").page(params[:page])
+    elsif !params[:search].nil?
       @customers = Customer.search(params[:search])
     else
       @customers = Customer.alphabetical_group(params[:letter]).page(params[:page])
